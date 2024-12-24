0eb99291694068f5f4a5d86c6c34a401db060c59
Sahin Aliyev <qaraxanli.sahin@gmail.com>
2024-12-24 15:01:06 +0400

    Added Area field

--- Runtime/Domain/AssetData.cs
+++ Runtime/Domain/AssetData.cs
@@ -1,10 +1,12 @@
 namespace DynamicBox.DynamicXRIntegration.Domain
 {
+	[System.Serializable]
 	public class AssetData
 	{
 		public long Id;
 		public string Name;
 		public bool IsAvailable;
 		public long Price;
+		public float Area;
 	}
 }
